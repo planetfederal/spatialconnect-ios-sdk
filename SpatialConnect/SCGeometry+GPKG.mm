79b5b21b21ebf0ef1ca57662e55baa2929f519a0
Wes Richardet <wes@tetriscodes.com>
2016-03-30 11:10:02 -0500

    Integrating libgpkg for gpgk vector data

--- SpatialConnect/SCGeometry+GPKG.mm
+++ SpatialConnect/SCGeometry+GPKG.mm
@@ -79,81 +79,17 @@ NSInteger const GPKG_GEO_PACKAGE_GEOMETRY_VERSION_1 = 0;
   if (reserved7 != 0 || reserved6 != 0) {
     [NSException raise:@"GPKGGeometry Flags" format:@"Unexpected GeoPackage Geometry flags. Flag bit 7 and 6 should both be 0, 7=%d, 6=%d", reserved7, reserved6];
   }
-
   int emptyValue = (flagsInt >> 4) & 1;
   empty = emptyValue == 1;
-
-  // Get the envelope contents indicator code (3-bit unsigned integer from
-  // bits 3, 2, and 1)
   int envelopeIndicator = (flagsInt >> 1) & 7;
   if (envelopeIndicator > 4) {
     [NSException raise:@"Geometry Flags" format:@"Unexpected GeoPackage Geometry flags. Envelope contents indicator must be between 0 and 4. Actual: %d", envelopeIndicator];
   }
-
-  // Get the byte order from bit 0, 0 for Big Endian and 1 for Little
-  // Endian
   int byteOrderValue = flagsInt & 1;
   CFByteOrder byteOrder = byteOrderValue == 0 ? CFByteOrderBigEndian
   : CFByteOrderLittleEndian;
-
   [reader setByteOrder:byteOrder];
-
-  // Read the 5th - 8th bytes as the srs id
   NSNumber *srsId = [reader readInt];
-
-  // Read the envelope
-  WKBGeometryEnvelope * envelope = nil;
-
-  if(envelopeIndicator > 0){
-
-    // Read x and y values and create envelope
-    NSDecimalNumber * minX = [reader readDouble];
-    NSDecimalNumber * maxX = [reader readDouble];
-    NSDecimalNumber * minY = [reader readDouble];
-    NSDecimalNumber * maxY = [reader readDouble];
-
-    BOOL hasZ = false;
-    NSDecimalNumber *  minZ = nil;
-    NSDecimalNumber *  maxZ = nil;
-
-    BOOL hasM = false;
-    NSDecimalNumber *  minM = nil;
-    NSDecimalNumber *  maxM = nil;
-
-    // Read z values
-    if (envelopeIndicator == 2 || envelopeIndicator == 4) {
-      hasZ = true;
-      minZ = [reader readDouble];
-      maxZ = [reader readDouble];
-    }
-
-    // Read m values
-    if (envelopeIndicator == 3 || envelopeIndicator == 4) {
-      hasM = true;
-      minM = [reader readDouble];
-      maxM = [reader readDouble];
-    }
-
-    envelope = [[WKBGeometryEnvelope alloc] initWithHasZ:hasZ andHasM:hasM];
-
-    [envelope setMinX:minX];
-    [envelope setMaxX:maxX];
-    [envelope setMinY:minY];
-    [envelope setMaxY:maxY];
-
-    if (hasZ) {
-      [envelope setMinZ:minZ];
-      [envelope setMaxZ:maxZ];
-    }
-
-    if (hasM) {
-      [envelope setMinM:minM];
-      [envelope setMaxM:maxM];
-    }
-
-  }
-
-  // Read the Well-Known Binary Geometry if not marked as empty
   WKBGeometry *geometry = nil;
   if(!empty){
     geometry = [WKBGeometryReader readGeometryWithReader:reader];
@@ -194,30 +130,19 @@ NSInteger const GPKG_GEO_PACKAGE_GEOMETRY_VERSION_1 = 0;
 -(NSData *)bytes
 {
   WKBByteWriter * writer = [[WKBByteWriter alloc] init];
-
-  // Write GP as the 2 byte magic number
   [writer writeString:GPKG_GEO_PACKAGE_GEOMETRY_MAGIC_NUMBER];
-
-  // Write a byte as the version, value of 0 = version 1
   [writer writeByte:[NSNumber numberWithInteger:GPKG_GEO_PACKAGE_GEOMETRY_VERSION_1]];
 
   int flag = 0;
 
-  // Add the binary type to bit 5, 0 for standard and 1 for extended
   BOOL extended = NO;
   int binaryType = extended ? 1 : 0;
   flag += (binaryType << 5);
-
-  // Add the empty geometry flag to bit 4, 0 for non-empty and 1 for
-  // empty
   BOOL empty = NO;
   int emptyValue = empty ? 1 : 0;
   flag += (emptyValue << 4);
 
-  // Add the envelope contents indicator code (3-bit unsigned integer to
-  // bits 3, 2, and 1)
-  WKBGeometryEnvelope *envelope = nil;
-  int envelopeIndicator = [self getIndicatorWithEnvelope:envelope];
+  int envelopeIndicator = 1;
   flag += (envelopeIndicator << 1);
 
   CFByteOrder byteOrder = CFByteOrderBigEndian;
@@ -228,31 +153,7 @@ NSInteger const GPKG_GEO_PACKAGE_GEOMETRY_VERSION_1 = 0;
   [writer writeByte:flags];
   [writer setByteOrder:byteOrder];
 
-  // Write the 4 byte srs id int
   [writer writeInt:self.srsId];
-
-  // Write the envelope
-  if (envelope != nil) {
-
-    // Write x and y values
-    [writer writeDouble:envelope.minX];
-    [writer writeDouble:envelope.maxX];
-    [writer writeDouble:envelope.minY];
-    [writer writeDouble:envelope.maxY];
-
-    // Write z values
-    if (envelope.hasZ) {
-      [writer writeDouble:envelope.minZ];
-      [writer writeDouble:envelope.maxZ];
-    }
-
-    // Write m values
-    if (envelope.hasM) {
-      [writer writeDouble:envelope.minM];
-      [writer writeDouble:envelope.maxM];
-    }
-  }
-
   if (!empty) {
     [WKBGeometryWriter writeGeometry:[self wkGeometry] withWriter:writer];
   }
